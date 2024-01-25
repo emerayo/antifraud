0749b17b9a6fdf8c9cfa3ab965173ec88a204b1b
Emanuel Merayo <emanuelmerayo@hotmail.com>
2024-01-24 22:11:33 -0300

    Create Api::V1::Transactions
    Actions SHOW and CREATE

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :transactions, only: %i[create show]
+    end
+  end
 end
