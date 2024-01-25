bae637802247e6e39caf3fe67609d764e07d6a43
Emanuel Merayo <emanuelmerayo@hotmail.com>
2024-01-24 22:40:10 -0300

    Add validations to Transaction model
    Increase coverage for Api::V1::Integrations endpoints

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Transaction, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:merchant) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:amount) }
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:card_number) }
+  end
 end
