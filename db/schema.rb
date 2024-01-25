0444e01aafa7d03a1c9f4f456df34561dd6e06f4
Emanuel Merayo <emanuelmerayo@hotmail.com>
2024-01-24 22:26:32 -0300

    Add has_cbk column to Transaction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_25_012130) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_25_012451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_25_012130) do
     t.float "amount", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "has_cbk", default: false, null: false
     t.index ["device_id"], name: "index_transactions_on_device_id"
     t.index ["merchant_id"], name: "index_transactions_on_merchant_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
